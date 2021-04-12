de5d16ac9599f5fc64b4ccd92e751f4c7822052a
sreekar-reddy541 <76102033+sreekar-reddy541@users.noreply.github.com>
2021-04-12 10:59:19 +0530

    Update complex_numbers_sum.c

--- complex_numbers_sum.c
+++ complex_numbers_sum.c
@@ -1,16 +1,33 @@
 //WAP to find the sum of n complex numbers using structures and 4 or more functions.
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int a[50],coun,sum=0;
+int countt()
 {
-int n,a[100],sum=0;
-printf("enter the total number\n");
-scanf("%d",&n);
-for(int i=0;i<n;i++)
+printf("Enter the count for the total numbers: ");
+scanf("%d",&coun);
+return coun;
+}
+int input(int coun)
 {
-printf("enter a new number\n");
+printf("Enter the Numbers to be added:");
+for(int i =0;i<coun;i++)
+{
 scanf("%d",&a[i]);
+}
+for(int i =0;i<coun;i++)
+{
 sum=sum+a[i];
 }
-printf("the final sum is %d\n",sum);
-return 0;
+return sum;
 }
+int display()
+{
+printf("The sum of the given numbers is: %d",sum);
+}
+int main()
+{
+int n=countt();
+int s =input(n);
+display(s);
+}
+
